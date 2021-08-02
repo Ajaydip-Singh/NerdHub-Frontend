e36da65f7f599afc1190213427acb946a7eba142
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-02 18:13:56 +0300

    :sparkles: feat: create header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Header
+      navItems={[
+        { name: "Login", to: "/login" },
+        { name: "Register", to: "/register" },
+      ]}
+    ></Header>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,57 @@
+import styles from "./Header.module.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+export default function Header(props) {
+  const nav_items = props.navItems ? props.navItems : null;
+
+  return (
+    <Router>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <ul className={styles.nav_list}>
+            <li className={styles.nav_list_item}>
+              <Link to="/">
+                <img
+                  src="/logo192.png"
+                  alt="Nerdhub Logo"
+                  className={styles.logo}
+                />
+              </Link>
+            </li>
+            <li className={styles.nav_list_item}>
+              <Link className={styles.nav_link} to="/">
+                Home
+              </Link>
+            </li>
+            <li className={styles.nav_list_item}>
+              <Link className={styles.nav_link} to="/gallery">
+                Gallery
+              </Link>
+            </li>
+            <li className={styles.nav_list_item}>
+              <Link className={styles.nav_link} to="/about">
+                About
+              </Link>
+            </li>
+            <li className={styles.nav_list_item}>
+              <Link className={styles.nav_link} to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {nav_items && (
+            <ul className={styles.nav_list}>
+              {nav_items.map((item) => (
+                <li className={styles.nav_list_item} key={item}>
+                  <Link className={styles.nav_link} to={item.to}>
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </nav>
+      </header>
+    </Router>
+  );
+}
