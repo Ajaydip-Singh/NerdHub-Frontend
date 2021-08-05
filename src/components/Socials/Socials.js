67ceb9f27b8e7c4c85dfa66eec79a26d9b9c015a
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-05 18:09:51 +0300

    :sparkles: feat: support dark and text social icons

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -1,6 +1,6 @@
 import styles from "./Socials.module.css";
 
-export default function Socials({ vertical }) {
+export default function Socials({ vertical, dark, text }) {
   return (
     <ul
       className={`align_center space_between ${styles.social_list} ${
@@ -8,19 +8,38 @@ export default function Socials({ vertical }) {
       }`}
     >
       <li className={styles.social_list_item}>
-        <a href="https://web.facebook.com/" className={styles.social_link}>
-          <i class="fab fa-facebook fa-2x"></i>
+        <a
+          href="https://web.facebook.com/"
+          className={`row_f align-center ${
+            dark ? styles.social_link_dark : styles.social_link
+          }`}
+        >
+          <i class="fab fa-facebook fa-2x fa-fw"></i>
+
+          {text ? "Facebook" : ""}
         </a>
       </li>
 
       <li className={styles.social_list_item}>
-        <a href="https://www.instagram.com/" className={styles.social_link}>
-          <i class="fab fa-instagram fa-2x"></i>
+        <a
+          href="https://www.instagram.com/"
+          className={`row_f align-center ${
+            dark ? styles.social_link_dark : styles.social_link
+          }`}
+        >
+          <i class="fab fa-instagram fa-2x fa-fw"></i>
+          {text ? "Instagram" : ""}
         </a>
       </li>
       <li className={styles.social_list_item}>
-        <a href="https://twitter.com/" className={styles.social_link}>
-          <i class="fab fa-twitter-square fa-2x"></i>
+        <a
+          href="https://twitter.com/"
+          className={`row_f align-center ${
+            dark ? styles.social_link_dark : styles.social_link
+          }`}
+        >
+          <i class="fab fa-twitter-square fa-2x fa-fw"></i>
+          {text ? "Twitter" : ""}
         </a>
       </li>
     </ul>
