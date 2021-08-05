a2d874714a73778a206236b6c1f992888680a335
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-05 16:44:29 +0300

    :sparkles: feat: add maps section in contact page

--- src/screens/ContactScreen/ContactScreen.js
+++ src/screens/ContactScreen/ContactScreen.js
@@ -11,7 +11,9 @@ export default function ContactScreen() {
       <Header contact></Header>
       <section
         className={styles.hero_section}
-        style={{ backgroundImage: "url(/images/masked_player_gaming_green.jpeg)" }}
+        style={{
+          backgroundImage: "url(/images/masked_player_gaming_green.jpeg)",
+        }}
       >
         <h1 className={styles.heading}>Contact us</h1>
       </section>
@@ -65,7 +67,25 @@ export default function ContactScreen() {
             </form>
           </div>
         </div>
+        <div className={`row align-center container ${styles.wrapper}`}>
+          <div className="col-md">
+            <iframe
+              title="location"
+              className={styles.map}
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.856189861609!2d36.8161384503457!3d-1.2583074990777114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f171b74f9ba57%3A0x11a8c077b37bd655!2sDiamond%20Plaza%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1628170305691!5m2!1sen!2ske"
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+          <div className="col-md">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+            ratione illo officiis omnis excepturi facere perspiciatis numquam
+            vero maiores harum accusantium, doloremque quidem adipisci vel
+            magnam inventore eos sunt? Eligendi.
+          </div>
+        </div>
       </section>
+      <section></section>
 
       <MediaQuery maxWidth={800}>
         <BottomNav contact></BottomNav>
