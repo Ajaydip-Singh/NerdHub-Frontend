573af24aeee652efd3c243e43a54e66fc543fcf9
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-19 16:48:00 +0300

    :art: chore: handle categories filter in events screen

--- src/screens/EventsScreen/EventsScreen.js
+++ src/screens/EventsScreen/EventsScreen.js
@@ -1,23 +1,44 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import MediaQuery from 'react-responsive';
 import BottomNav from '../../components/BottomNav/BottomNav';
 import Event from '../../components/Event/Event';
-import FilterBox from '../../components/FilterBox/FilterBox';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import LoadingBox from '../../components/LoadingBox/LoadingBox';
 import MessageBox from '../../components/MessageBox/MessageBox';
+import { getEventsCategories } from '../../slices/eventSlices/eventsCategoriesGetSlice';
 import { getEvents } from '../../slices/eventSlices/eventsGetSlice';
 import styles from './EventsScreen.module.css';
 
-export default function EventsScreen() {
+export default function EventsScreen(props) {
+  const [defaultCategory, setDefaultCategory] = useState('all');
+
   const eventsGetSlice = useSelector((state) => state.eventsGetSlice);
   const { status, events, error } = eventsGetSlice;
 
+  const eventsCategoriesGetSlice = useSelector(
+    (state) => state.eventsCategoriesGetSlice
+  );
+  const { categories } = eventsCategoriesGetSlice;
+
+  const venues = [...new Set(events.map((event) => event.venue))];
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   const dispatch = useDispatch();
+
+  const handleCategoryChange = (e) => {
+    const category = e.target.value;
+    dispatch(getEvents({ category: category === 'all' ? '' : category }));
+    setDefaultCategory(category);
+  };
+
   useEffect(() => {
     dispatch(getEvents({}));
+    dispatch(getEventsCategories());
   }, [dispatch]);
 
   return (
@@ -30,7 +51,46 @@ export default function EventsScreen() {
         }}
       >
         <div className={styles.filterbox}>
-          <FilterBox events={events}></FilterBox>
+          <div className={styles.wrapper}>
+            <form className={styles.search} onSubmit={submitHandler}>
+              <div className="row_f">
+                <input
+                  className={styles.input}
+                  type="text"
+                  name="q"
+                  placeholder="Search event by name"
+                  id="q"
+                />
+                <button type="submit" className={styles.search_button}>
+                  <i className="fa fa-search"></i>
+                </button>
+              </div>
+            </form>
+            <div className={styles.filter_button_wrapper}>
+              <select
+                className={`${styles.search_button} ${styles.filter_button}`}
+                value={defaultCategory}
+                onChange={handleCategoryChange}
+              >
+                <option value="all">All Categories</option>
+                {categories &&
+                  categories.map((category) => (
+                    <option value={category}>{category}</option>
+                  ))}
+              </select>
+              <select
+                className={`${styles.search_button} ${styles.filter_button}`}
+              >
+                <option value="">All Venues</option>
+                {venues &&
+                  venues.map((venue) => (
+                    <>
+                      <option value={venue}>{venue}</option>
+                    </>
+                  ))}
+              </select>
+            </div>
+          </div>
         </div>
         {status === 'loading' ? (
           <LoadingBox></LoadingBox>
