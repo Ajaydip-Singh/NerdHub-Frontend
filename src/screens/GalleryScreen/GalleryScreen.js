075a5ae39d4a13fb681ddfb7c21c07f15834484c
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-21 13:31:13 +0300

    :lipstick: style: show gallery in slideshow view

--- src/screens/GalleryScreen/GalleryScreen.js
+++ src/screens/GalleryScreen/GalleryScreen.js
@@ -1,21 +1,165 @@
-import MediaQuery from "react-responsive";
-import BottomNav from "../../components/BottomNav/BottomNav";
-import Footer from "../../components/Footer/Footer";
-import Header from "../../components/Header/Header";
-import styles from "./GalleryScreen.module.css";
+import MediaQuery from 'react-responsive';
+import BottomNav from '../../components/BottomNav/BottomNav';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import styles from './GalleryScreen.module.css';
+import ImageGallery from 'react-image-gallery';
 
 export default function GalleryScreen() {
+  const images = [
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1015/250/150/'
+    },
+    {
+      original: 'https://picsum.photos/id/1019/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1019/250/150/'
+    }
+  ];
+
+  const renderLeftNav = (onClick, disabled) => (
+    <span
+      onClick={onClick}
+      disabled={disabled}
+      aria-hidden="true"
+      className={`carousel-control-prev-icon ${styles.prev_icon}`}
+    />
+  );
+
+  const renderRightNav = (onClick, disabled) => (
+    <span
+      onClick={onClick}
+      disabled={disabled}
+      aria-hidden="true"
+      className={`carousel-control-next-icon ${styles.next_icon}`}
+    />
+  );
+
   return (
     <div>
       <Header gallery></Header>
+
       <div
         className={styles.main_wrapper}
         style={{
-          backgroundImage: "url(/images/destruction_long.jpeg)",
-          width: "100%",
-          height: "200vh",
+          // backgroundImage: 'url(/images/destruction_long.jpeg)',
+          width: '100%'
         }}
-      ></div>
+      >
+        <div className={styles.gallery}>
+          <ImageGallery
+            renderLeftNav={renderLeftNav}
+            renderRightNav={renderRightNav}
+            showFullscreenButton={false}
+            lazyLoad={true}
+            items={images}
+          />
+        </div>
+      </div>
 
       <MediaQuery minWidth={800}>
         <Footer></Footer>
