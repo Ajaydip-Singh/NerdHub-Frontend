124e22312538813ba65fa26167fcb0c9e3158b9e
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-24 17:40:25 +0300

    :sparkles: feat: add function to strip html from string

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,7 @@ export const formatDate = (date) => {
   const dateObj = new Date(date);
   return dateObj.toDateString();
 };
+
+export const stripHtml = (html) => {
+  return html.replace(/<\s*[^>]*>/gi, '');
+};
