e84d6a9b9b10346f9492127e537a8b350b39be90
Ajaydip Singh <singh@minerva.kgi.edu>
2021-08-31 11:07:25 +0300

    :art: chore: add more features to quill editor

--- src/components/TextEditor/TextEditor.js
+++ src/components/TextEditor/TextEditor.js
@@ -6,24 +6,46 @@ export default function TextEditor(props) {
   const modules = {
     toolbar: [
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ size: ['small', false, 'large', 'huge'] }],
       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ list: 'ordered' }, { list: 'bullet' }],
+      [
+        { align: '' },
+        { align: 'center' },
+        { align: 'right' },
+        { align: 'justify' }
+      ],
+      [({ list: 'ordered' }, { list: 'bullet' })],
       [{ color: ['white', 'black', 'red', 'blue', '#50d450'] }],
+      [
+        {
+          background: [
+            'white',
+            'black',
+            'red',
+            'blue',
+            'transparent',
+            '#50d450'
+          ]
+        }
+      ],
       ['clean']
     ]
   };
 
   const formats = [
     'header',
+    'size',
     'bold',
     'italic',
     'underline',
     'strike',
     'blockquote',
     'list',
+    'align',
     'bullet',
     'link',
-    'color'
+    'color',
+    'background'
   ];
 
   return (
