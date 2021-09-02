e43491c3b79a2286d68a46ef6a6a2752380f2d72
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-02 09:20:51 +0300

    :sparkles: feat: add animation for home screen

---
+++ src/animate.js
@@ -0,0 +1,25 @@
+export const pageVariant = {
+  initial: {
+    opacity: 0,
+    scale: 1.05
+  },
+  final: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 0.1,
+      duration: 1
+    }
+  }
+};
+
+export const sectionVariant = {
+  hover: {
+    scale: 0.95,
+    borderRadius: '10px',
+    border: '2px solid #50d450'
+  },
+  transition: {
+    duration: 1
+  }
+};
--- src/screens/PublicScreens/HomeScreen/HomeScreen.js
+++ src/screens/PublicScreens/HomeScreen/HomeScreen.js
@@ -1,4 +1,6 @@
 import MediaQuery, { useMediaQuery } from 'react-responsive';
+import parse from 'html-react-parser';
+import { motion } from 'framer-motion';
 import BottomNav from '../../../components/BottomNav/BottomNav';
 import Header from '../../../components/Header/Header';
 import Socials from '../../../components/Socials/Socials';
@@ -14,7 +16,7 @@ import Event from '../../../components/Event/Event';
 import LoadingBox from '../../../components/LoadingBox/LoadingBox';
 import CustomCarousel from '../../../components/CustomCarousel/CustomCarousel';
 import { getHomePageContent } from '../../../slices/pageSlices/homePageContentSlices/homePageContentGetSlice';
-import parse from 'html-react-parser';
+import { pageVariant, sectionVariant } from '../../../animate';
 
 export default function HomeScreen() {
   const isSmallerScreen = useMediaQuery({ query: '(max-width: 800px)' });
@@ -53,189 +55,210 @@ export default function HomeScreen() {
   return (
     <div>
       <Header home></Header>
-      <section className={styles.hero_section}>
-        <Carousel
-          fade
-          indicators={false}
-          nextIcon={nextIcon}
-          prevIcon={prevIcon}
+      <motion.div variants={pageVariant} initial="initial" animate="final">
+        <motion.div
+          className={styles.hero_section}
+          variants={sectionVariant}
+          whileHover="hover"
+          transition="transition"
         >
-          <Carousel.Item>
-            <img
-              className={`d-block w-100 ${styles.carousel_image} ${
-                isSmallerScreen
-                  ? `${styles.image_small_screen}`
-                  : `${styles.image_large_screen}`
-              }`}
-              src="/images/gaming_room.jpeg"
-              alt="First slide"
-            />
-            <Carousel.Caption className={styles.carousel_caption}>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className={`d-block w-100 ${styles.carousel_image} ${
-                isSmallerScreen
-                  ? `${styles.image_small_screen}`
-                  : `${styles.image_large_screen}`
-              }`}
-              src="/images/player_gaming.jpeg"
-              alt="Second slide"
-            />
-
-            <Carousel.Caption className={styles.carousel_caption}>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </section>
-      <MediaQuery minWidth={800}>
-        <div className={styles.social_icons}>
-          <Socials vertical />
-        </div>
-      </MediaQuery>
-      <section
-        style={{
-          backgroundImage: 'url(/images/many_fighters_green.png)'
-        }}
-        className={styles.video_section}
-      >
-        <div className={styles.video_container}>
-          <div>
-            <div className={styles.section_heading}>
-              {content
-                ? parse(content.videoHeading)
-                : `
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum,
-              cupiditate.`}
-            </div>
-          </div>
-          <div
-            className={styles.video}
-            style={{
-              border: content
-                ? `2px solid ${content.videoBorderColor}`
-                : `2px solid #000`,
-              boxShadow: content
-                ? `6px 6px 6px ${content.videoBoxShadowColor}`
-                : `6px 6px 6px #000;`
-            }}
+          <Carousel
+            fade
+            indicators={false}
+            nextIcon={nextIcon}
+            prevIcon={prevIcon}
           >
-            <Player
-              poster={
-                content ? content.videoThumbnail : `/images/knife_dark.jpeg`
-              }
-              src={
-                content
-                  ? content.videoUrl
-                  : `https://media.w3.org/2010/05/sintel/trailer_hd.mp4`
-              }
-            ></Player>
-          </div>
-        </div>
-      </section>
-      {status === 'loading' ? (
-        <LoadingBox></LoadingBox>
-      ) : error ? (
-        ''
-      ) : (
-        <section>
-          <div
-            className={styles.events_slider_background}
-            style={{
-              backgroundImage: 'url(/images/fighters_dark.jpeg)',
-              backgroundSize: 'cover'
-            }}
-          >
-            <h2 className={styles.events_section_heading}>
-              Upcoming{' '}
-              <Link className={styles.link} to="/events">
-                Events
-              </Link>
-            </h2>
-            <CustomCarousel
-              disableButtons={events && events.length < 2}
-              disableDots={events && events.length < 2}
-              infinite={events && events.length > 1}
-              items={events.map((event, index) => (
-                <Event order={index} event={event}></Event>
-              ))}
-            ></CustomCarousel>
-          </div>
-        </section>
-      )}
-
-      <div
-        className={styles.contact_background}
-        style={{
-          backgroundColor: content ? content.contactBackgroundColor : '#000'
-        }}
-      >
-        <div className={`row container ${styles.contact_wrapper}`}>
-          <div className="col-md">
-            <div>
-              {content ? (
-                parse(content.contactText)
-              ) : (
-                <p className={styles.contact_info}>
-                  Contact us about{' '}
-                  <span className="green">press matters, </span>
-                  potential <span className="green">sponsorships, </span>
-                  and
-                  <span className="green"> membership </span>inquiries.
+            <Carousel.Item>
+              <img
+                className={`d-block w-100 ${styles.carousel_image} ${
+                  isSmallerScreen
+                    ? `${styles.image_small_screen}`
+                    : `${styles.image_large_screen}`
+                }`}
+                src="/images/gaming_room.jpeg"
+                alt="First slide"
+              />
+              <Carousel.Caption className={styles.carousel_caption}>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
                 </p>
-              )}
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className={`d-block w-100 ${styles.carousel_image} ${
+                  isSmallerScreen
+                    ? `${styles.image_small_screen}`
+                    : `${styles.image_large_screen}`
+                }`}
+                src="/images/player_gaming.jpeg"
+                alt="Second slide"
+              />
+
+              <Carousel.Caption className={styles.carousel_caption}>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </motion.div>
+        <MediaQuery minWidth={800}>
+          <div className={styles.social_icons}>
+            <Socials vertical />
+          </div>
+        </MediaQuery>
+        <motion.div
+          variants={sectionVariant}
+          whileHover="hover"
+          transition="transition"
+          style={{
+            backgroundImage: 'url(/images/many_fighters_green.png)'
+          }}
+          className={styles.video_section}
+        >
+          <div className={styles.video_container}>
+            <div>
+              <div className={styles.section_heading}>
+                {content && parse(content.videoHeading)}
+              </div>
             </div>
 
-            <Link className="link border_bottom" to="/about">
-              Learn more about us
-            </Link>
-            <div className={styles.socials}>
-              <Socials></Socials>
+            <motion.div
+              whileHover={{
+                scale: 1.02,
+                borderColor: '#fff'
+              }}
+              transition={{ type: 'tween', duration: 0.8 }}
+              className={styles.video}
+              style={{
+                border: content
+                  ? `2px solid ${content.videoBorderColor}`
+                  : `2px solid #000`,
+                boxShadow: content
+                  ? `6px 6px 6px ${content.videoBoxShadowColor}`
+                  : `6px 6px 6px #000;`
+              }}
+            >
+              <Player
+                poster={
+                  content ? content.videoThumbnail : `/images/knife_dark.jpeg`
+                }
+                src={
+                  content
+                    ? content.videoUrl
+                    : `https://media.w3.org/2010/05/sintel/trailer_hd.mp4`
+                }
+              ></Player>
+            </motion.div>
+          </div>
+        </motion.div>
+        {status === 'loading' ? (
+          <LoadingBox></LoadingBox>
+        ) : error ? (
+          ''
+        ) : (
+          <motion.section
+            variants={sectionVariant}
+            whileHover="hover"
+            transition="transition"
+          >
+            <div
+              className={styles.events_slider_background}
+              style={{
+                backgroundImage: 'url(/images/fighters_dark.jpeg)',
+                backgroundSize: 'cover'
+              }}
+            >
+              <h2 className={styles.events_section_heading}>
+                Upcoming{' '}
+                <Link className={styles.link} to="/events">
+                  Events
+                </Link>
+              </h2>
+              <CustomCarousel
+                disableButtons={events && events.length < 2}
+                disableDots={events && events.length < 2}
+                infinite={events && events.length > 1}
+                items={events.map((event, index) => (
+                  <Event order={index} event={event}></Event>
+                ))}
+              ></CustomCarousel>
+            </div>
+          </motion.section>
+        )}
+
+        <motion.div
+          variants={sectionVariant}
+          whileHover="hover"
+          transition="transition"
+          className={styles.contact_background}
+          style={{
+            backgroundColor: content ? content.contactBackgroundColor : '#000'
+          }}
+        >
+          <div className={`row container ${styles.contact_wrapper}`}>
+            <div className="col-md">
+              <div>
+                {content ? (
+                  parse(content.contactText)
+                ) : (
+                  <p className={styles.contact_info}>
+                    Contact us about{' '}
+                    <span className="green">press matters, </span>
+                    potential <span className="green">sponsorships, </span>
+                    and
+                    <span className="green"> membership </span>inquiries.
+                  </p>
+                )}
+              </div>
+
+              <Link className="link border_bottom" to="/about">
+                Learn more about us
+              </Link>
+              <div className={styles.socials}>
+                <Socials></Socials>
+              </div>
+            </div>
+            <div className="col-md">
+              <form action="" className={styles.form}>
+                <div>
+                  <textarea
+                    className={styles.textarea}
+                    placeholder="Write Message"
+                    name=""
+                    id=""
+                    rows="5"
+                  ></textarea>
+                </div>
+                <div className={`row_f space-between ${styles.inputs_wrapper}`}>
+                  <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="Your Name"
+                  />
+                  <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="Your Email"
+                  />
+                </div>
+                <div>
+                  <button className={styles.submit_button} type="submit">
+                    Submit message
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
-          <div className="col-md">
-            <form action="" className={styles.form}>
-              <div>
-                <textarea
-                  className={styles.textarea}
-                  placeholder="Write Message"
-                  name=""
-                  id=""
-                  rows="5"
-                ></textarea>
-              </div>
-              <div className={`row_f space-between ${styles.inputs_wrapper}`}>
-                <input
-                  className={styles.input}
-                  type="text"
-                  placeholder="Your Name"
-                />
-                <input
-                  className={styles.input}
-                  type="text"
-                  placeholder="Your Email"
-                />
-              </div>
-              <div>
-                <button className={styles.submit_button} type="submit">
-                  Submit message
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      <MediaQuery minWidth={800}>
-        <Footer></Footer>
-      </MediaQuery>
-      <MediaQuery maxWidth={800}>
-        <BottomNav home></BottomNav>
-      </MediaQuery>
+        </motion.div>
+        <MediaQuery minWidth={800}>
+          <Footer></Footer>
+        </MediaQuery>
+        <MediaQuery maxWidth={800}>
+          <BottomNav home></BottomNav>
+        </MediaQuery>
+      </motion.div>
     </div>
   );
 }
