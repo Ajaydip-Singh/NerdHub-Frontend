4da1b929a7ac4a2f24399ade0c9b2ca6b3542641
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-03 21:45:01 +0300

    :lipstick: style: add tile animation on hover for about page

--- src/screens/PublicScreens/AboutScreen/AboutScreen.js
+++ src/screens/PublicScreens/AboutScreen/AboutScreen.js
@@ -9,7 +9,7 @@ import { useEffect } from 'react';
 import { getAboutPageContent } from '../../../slices/pageSlices/aboutPageContentSlices/aboutPageContentGetSlice';
 import parse from 'html-react-parser';
 import { motion } from 'framer-motion';
-import { pageVariant, videoVariant } from '../../../animate';
+import { pageVariant, sectionVariant, videoVariant } from '../../../animate';
 
 export default function AboutScreen() {
   const aboutPageContentGetSlice = useSelector(
@@ -40,6 +40,8 @@ export default function AboutScreen() {
           initial={{ opacity: 0, x: '-100vw' }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1.2 }}
+          variants={sectionVariant}
+          whileHover="hover"
         >
           <motion.div
             drag
@@ -56,52 +58,104 @@ export default function AboutScreen() {
             )}
           </motion.div>
         </motion.section>
-        <section className={styles.video_section}>
-          <div className={styles.video_container}>
-            <motion.div
-              className={styles.video}
-              variants={videoVariant}
-              whileHover="hover"
-              transition="transition"
-              style={{
-                border: content
-                  ? `2px solid ${content.videoBorderColor}`
-                  : `2px solid #000`,
-                boxShadow: content
-                  ? `6px 6px 6px ${content.videoBoxShadowColor}`
-                  : `6px 6px 6px #000;`
-              }}
+        <motion.div
+          variants={sectionVariant}
+          whileHover="hover"
+          transition={{ duration: 1.2 }}
+        >
+          <section className={styles.video_section}>
+            <div className={styles.video_container}>
+              <motion.div
+                className={styles.video}
+                variants={videoVariant}
+                whileHover="hover"
+                transition="transition"
+                style={{
+                  border: content
+                    ? `2px solid ${content.videoBorderColor}`
+                    : `2px solid #000`,
+                  boxShadow: content
+                    ? `6px 6px 6px ${content.videoBoxShadowColor}`
+                    : `6px 6px 6px #000;`
+                }}
+              >
+                <Player
+                  poster={
+                    content ? content.videoThumbnail : `/images/knife_dark.jpeg`
+                  }
+                  src={
+                    content
+                      ? content.videoUrl
+                      : `https://media.w3.org/2010/05/sintel/trailer_hd.mp4`
+                  }
+                ></Player>
+              </motion.div>
+            </div>
+          </section>
+          <section>
+            <div
+              className={`row container align-center space-evenly ${styles.wrapper}`}
             >
-              <Player
-                poster={
-                  content ? content.videoThumbnail : `/images/knife_dark.jpeg`
-                }
-                src={
-                  content
-                    ? content.videoUrl
-                    : `https://media.w3.org/2010/05/sintel/trailer_hd.mp4`
-                }
-              ></Player>
-            </motion.div>
-          </div>
-        </section>
-        <section>
-          <div
-            className={`row container align-center space-evenly ${styles.wrapper}`}
-          >
-            <div className={styles.info_wrapper}>
-              <motion.div whileHover={{ scale: 0.9 }}>
+              <div className={styles.info_wrapper}>
+                <motion.div whileHover={{ scale: 0.9 }}>
+                  <div>
+                    {content ? (
+                      <div className="ql-editor">
+                        {parse(content.sectionOneHeading)}
+                      </div>
+                    ) : (
+                      <h1>What we do</h1>
+                    )}
+                    {content ? (
+                      <div className="ql-editor">
+                        {parse(content.sectionOneText)}
+                      </div>
+                    ) : (
+                      <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Magni consectetur, dolor illum aperiam repellat
+                        numquam iure, laudantium sunt tenetur aut quasi eum vel
+                        maxime deleniti corporis quod nemo totam optio.
+                      </p>
+                    )}
+                  </div>
+                </motion.div>
+                <div className={styles.image_container}>
+                  <motion.img
+                    whileHover={{ scale: 0.8 }}
+                    drag
+                    dragConstraints={{
+                      top: 10,
+                      left: 10,
+                      right: 10,
+                      bottom: 10
+                    }}
+                    dragElastic={1}
+                    src="/logo512.png"
+                    className={styles.logo}
+                    alt="Nerdhub Logo"
+                  />
+                </div>
+              </div>
+              <div className={styles.info_wrapper}>
+                <div className={styles.image_container}>
+                  <img
+                    src="/images/gaming_room.jpeg"
+                    className={styles.image}
+                    alt="Nerdhub Logo"
+                  />
+                </div>
                 <div>
                   {content ? (
                     <div className="ql-editor">
-                      {parse(content.sectionOneHeading)}
+                      {parse(content.sectionTwoHeading)}
                     </div>
                   ) : (
-                    <h1>What we do</h1>
+                    <h1>Our Mission</h1>
                   )}
                   {content ? (
                     <div className="ql-editor">
-                      {parse(content.sectionOneText)}
+                      {parse(content.sectionTwoText)}
                     </div>
                   ) : (
                     <p>
@@ -112,51 +166,10 @@ export default function AboutScreen() {
                     </p>
                   )}
                 </div>
-              </motion.div>
-              <div className={styles.image_container}>
-                <motion.img
-                  whileHover={{ scale: 0.8 }}
-                  drag
-                  dragConstraints={{ top: 10, left: 10, right: 10, bottom: 10 }}
-                  dragElastic={1}
-                  src="/logo512.png"
-                  className={styles.logo}
-                  alt="Nerdhub Logo"
-                />
               </div>
             </div>
-            <div className={styles.info_wrapper}>
-              <div className={styles.image_container}>
-                <img
-                  src="/images/gaming_room.jpeg"
-                  className={styles.image}
-                  alt="Nerdhub Logo"
-                />
-              </div>
-              <div>
-                {content ? (
-                  <div className="ql-editor">
-                    {parse(content.sectionTwoHeading)}
-                  </div>
-                ) : (
-                  <h1>Our Mission</h1>
-                )}
-                {content ? (
-                  <div className="ql-editor">
-                    {parse(content.sectionTwoText)}
-                  </div>
-                ) : (
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Magni consectetur, dolor illum aperiam repellat numquam
-                    iure, laudantium sunt tenetur aut quasi eum vel maxime
-                    deleniti corporis quod nemo totam optio.
-                  </p>
-                )}
-              </div>
-            </div>
-          </div>
-        </section>
+          </section>
+        </motion.div>
       </motion.div>
       <MediaQuery minWidth={800}>
         <Footer></Footer>
