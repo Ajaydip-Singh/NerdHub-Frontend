7f5eead649696eb425d47474a1dbb07153c169d3
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-08 16:55:10 +0300

    :art: chore: only show events screen when events load

--- src/screens/PublicScreens/EventsScreen/EventsScreen.js
+++ src/screens/PublicScreens/EventsScreen/EventsScreen.js
@@ -58,88 +58,102 @@ export default function EventsScreen(props) {
   return (
     <div className={styles.screen}>
       <Header events></Header>
-      <motion.div
-        variants={pageVariant}
-        initial="initial"
-        animate="final"
-        className={styles.main_wrapper}
-        style={{
-          backgroundImage: 'url(/images/cubes.jpeg)'
-        }}
-      >
-        <motion.div
-          initial={{ x: '-100vw' }}
-          animate={{ x: 0 }}
-          whileHover={{ scale: 1.1 }}
-          transition={{ duration: 1 }}
-          className={styles.filterbox}
-        >
-          <div className={styles.wrapper}>
-            <form className={styles.search} onSubmit={submitHandler}>
-              <div className="row_f">
-                <input
-                  className={styles.input}
-                  type="text"
-                  name="q"
-                  value={inputEventName}
-                  onChange={(e) => setInputEventName(e.target.value)}
-                  placeholder="Search event by name"
-                  id="q"
-                />
-                <button
-                  type="submit"
-                  className={styles.search_button}
-                  onClick={() => setSearchEventName(inputEventName)}
-                >
-                  <i className="fa fa-search"></i>
-                </button>
-              </div>
-              <div className={styles.filter_button_wrapper}>
-                <select
-                  className={`${styles.search_button} ${styles.filter_button}`}
-                  value={category}
-                  onChange={(e) => setCategory(e.target.value)}
-                >
-                  <option value="all">All Categories</option>
-                  {categories &&
-                    categories.map((category) => (
-                      <option value={category}>{category}</option>
-                    ))}
-                </select>
-                <select
-                  className={`${styles.search_button} ${styles.filter_button}`}
-                  value={venue}
-                  onChange={(e) => setVenue(e.target.value)}
-                >
-                  <option value="all">All Venues</option>
-                  {venues &&
-                    venues.map((venue) => (
-                      <option value={venue}>{venue}</option>
-                    ))}
-                </select>
-              </div>
-            </form>
-          </div>
-        </motion.div>
-        <div className={styles.events_wrapper}>
-          {status === 'loading' ? (
-            <LoadingBox></LoadingBox>
-          ) : error ? (
-            <MessageBox variant="danger">{error}</MessageBox>
-          ) : (
-            events.map(
-              (event, index) =>
-                event.isActive && (
-                  <Event
-                    screen={'eventScreen'}
-                    order={index}
-                    event={event}
-                  ></Event>
-                )
-            )
-          )}
+      {status === 'loading' ? (
+        <div className="min_page_height">
+          <LoadingBox></LoadingBox>
         </div>
-      </motion.div>
+      ) : error ? (
+        <div className="min_page_height">
+          <MessageBox variant="danger">
+            Oops. We are temporarily unavailable. Please try again later.
+          </MessageBox>
+        </div>
+      ) : (
+        <>
+          <motion.div
+            variants={pageVariant}
+            initial="initial"
+            animate="final"
+            className={styles.main_wrapper}
+            style={{
+              backgroundImage: 'url(/images/cubes.jpeg)'
+            }}
+          >
+            <motion.div
+              initial={{ x: '-100vw' }}
+              animate={{ x: 0 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 1 }}
+              className={styles.filterbox}
+            >
+              <div className={styles.wrapper}>
+                <form className={styles.search} onSubmit={submitHandler}>
+                  <div className="row_f">
+                    <input
+                      className={styles.input}
+                      type="text"
+                      name="q"
+                      value={inputEventName}
+                      onChange={(e) => setInputEventName(e.target.value)}
+                      placeholder="Search event by name"
+                      id="q"
+                    />
+                    <button
+                      type="submit"
+                      className={styles.search_button}
+                      onClick={() => setSearchEventName(inputEventName)}
+                    >
+                      <i className="fa fa-search"></i>
+                    </button>
+                  </div>
+                  <div className={styles.filter_button_wrapper}>
+                    <select
+                      className={`${styles.search_button} ${styles.filter_button}`}
+                      value={category}
+                      onChange={(e) => setCategory(e.target.value)}
+                    >
+                      <option value="all">All Categories</option>
+                      {categories &&
+                        categories.map((category) => (
+                          <option value={category}>{category}</option>
+                        ))}
+                    </select>
+                    <select
+                      className={`${styles.search_button} ${styles.filter_button}`}
+                      value={venue}
+                      onChange={(e) => setVenue(e.target.value)}
+                    >
+                      <option value="all">All Venues</option>
+                      {venues &&
+                        venues.map((venue) => (
+                          <option value={venue}>{venue}</option>
+                        ))}
+                    </select>
+                  </div>
+                </form>
+              </div>
+            </motion.div>
+            <div className={styles.events_wrapper}>
+              {status === 'loading' ? (
+                <LoadingBox></LoadingBox>
+              ) : error ? (
+                <MessageBox variant="danger">{error}</MessageBox>
+              ) : (
+                events.map(
+                  (event, index) =>
+                    event.isActive && (
+                      <Event
+                        screen={'eventScreen'}
+                        order={index}
+                        event={event}
+                      ></Event>
+                    )
+                )
+              )}
+            </div>
+          </motion.div>
+        </>
+      )}
 
       <MediaQuery minWidth={800}>
         <Footer></Footer>
