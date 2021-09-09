026925952257f4db532dbe8103fdff396cffe909
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-09 21:59:11 +0300

    :lipstick: style: make products card dark

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,22 +1,27 @@
 import { Link } from 'react-router-dom';
-import Rating from './Rating';
+import Rating from '../Rating/Rating';
 import styles from './Product.module.css';
+import { motion } from 'framer-motion';
 
 export default function Product(props) {
   const { product } = props;
 
   return (
-    <div key={product._id} className={styles.card}>
+    <motion.div
+      key={product._id}
+      className={styles.card}
+      whileHover={{
+        scale: 1.02
+      }}
+    >
       <Link to={`/shop/product/${product._id}`}>
         <img className={styles.medium} src={product.image} alt={product.name} />
       </Link>
       <div className={styles.card_body}>
-        <Link to={`/shop/product/${product._id}`}>
-          <h2>{product.name}</h2>
-        </Link>
+        <h2 className={styles.product_name}>{product.name}</h2>
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <div className={styles.price}>${product.price}</div>
       </div>
-    </div>
+    </motion.div>
   );
 }
