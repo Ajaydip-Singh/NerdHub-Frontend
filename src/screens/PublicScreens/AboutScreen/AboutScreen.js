3ef3b6045d02a0ec35e4a11e6d6a12fcbe256a9d
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-09 22:48:58 +0300

    :recycle: refactor: show images from backend

--- src/screens/PublicScreens/AboutScreen/AboutScreen.js
+++ src/screens/PublicScreens/AboutScreen/AboutScreen.js
@@ -10,12 +10,14 @@ import { getAboutPageContent } from '../../../slices/pageSlices/aboutPageContent
 import parse from 'html-react-parser';
 import { motion } from 'framer-motion';
 import { pageVariant, sectionVariant, videoVariant } from '../../../animate';
+import LoadingBox from '../../../components/LoadingBox/LoadingBox';
+import MessageBox from '../../../components/MessageBox/MessageBox';
 
 export default function AboutScreen() {
   const aboutPageContentGetSlice = useSelector(
     (state) => state.aboutPageContentGetSlice
   );
-  const { content } = aboutPageContentGetSlice;
+  const { status, content, error } = aboutPageContentGetSlice;
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -26,151 +28,136 @@ export default function AboutScreen() {
     <div className={styles.screen}>
       <Header about></Header>
 
-      <motion.div
-        variants={pageVariant}
-        initial="initial"
-        animate="final"
-        className={styles.main_wrapper}
-        style={{
-          backgroundImage: 'url(images/fighters.jpeg)'
-        }}
-      >
-        <motion.section
-          className={styles.hero_section}
-          initial={{ opacity: 0, x: '-100vw' }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1.2 }}
-          variants={sectionVariant}
-          whileHover="hover"
-        >
-          <motion.div
-            drag
-            dragConstraints={{ top: 10, left: 10, right: 10, bottom: 10 }}
-            dragTransition={{ bounceStiffness: 200, bounceDamping: 10 }}
-            whileHover={{ x: 1.5, scale: 1.2 }}
-            transition={{ yoyo: 5 }}
-            whileDrag={{ scale: 1.2 }}
-          >
-            {content ? (
-              <div className="ql-editor">{parse(content.aboutMainHeading)}</div>
-            ) : (
-              <h1 className={styles.heading}>About</h1>
-            )}
-          </motion.div>
-        </motion.section>
+      {status === 'loading' ? (
+        <div className="min_page_height">
+          <LoadingBox></LoadingBox>
+        </div>
+      ) : error ? (
+        <div className="min_page_height">
+          <MessageBox variant="danger">
+            Oops. We are temporarily unavailable. Please try again later.
+          </MessageBox>
+        </div>
+      ) : (
         <motion.div
-          variants={sectionVariant}
-          whileHover="hover"
-          transition={{ duration: 1.2 }}
+          variants={pageVariant}
+          initial="initial"
+          animate="final"
+          className={styles.main_wrapper}
+          style={{
+            backgroundImage: `url(${content && content.aboutBackgroundImage})`
+          }}
         >
-          <section className={styles.video_section}>
-            <div className={styles.video_container}>
-              <motion.div
-                className={styles.video}
-                variants={videoVariant}
-                whileHover="hover"
-                transition="transition"
-                style={{
-                  border: content
-                    ? `2px solid ${content.videoBorderColor}`
-                    : `2px solid #000`,
-                  boxShadow: content
-                    ? `6px 6px 6px ${content.videoBoxShadowColor}`
-                    : `6px 6px 6px #000;`
-                }}
-              >
-                <Player
-                  poster={
-                    content ? content.videoThumbnail : `/images/knife_dark.jpeg`
-                  }
-                  src={
-                    content
-                      ? content.videoUrl
-                      : `https://media.w3.org/2010/05/sintel/trailer_hd.mp4`
-                  }
-                ></Player>
-              </motion.div>
-            </div>
-          </section>
-          <section>
-            <div
-              className={`row container align-center space-evenly ${styles.wrapper}`}
+          <motion.section
+            className={styles.hero_section}
+            initial={{ opacity: 0, x: '-100vw' }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.2 }}
+            variants={sectionVariant}
+            whileHover="hover"
+          >
+            <motion.div
+              drag
+              dragConstraints={{ top: 10, left: 10, right: 10, bottom: 10 }}
+              dragTransition={{ bounceStiffness: 200, bounceDamping: 10 }}
+              whileHover={{ x: 1.5, scale: 1.2 }}
+              transition={{ yoyo: 5 }}
+              whileDrag={{ scale: 1.2 }}
             >
-              <div className={styles.info_wrapper}>
-                <motion.div whileHover={{ scale: 0.9 }}>
+              {content && (
+                <div className="ql-editor">
+                  {parse(content.aboutMainHeading)}
+                </div>
+              )}
+            </motion.div>
+          </motion.section>
+          <motion.div
+            variants={sectionVariant}
+            whileHover="hover"
+            transition={{ duration: 1.2 }}
+          >
+            <section className={styles.video_section}>
+              <div className={styles.video_container}>
+                <motion.div
+                  className={styles.video}
+                  variants={videoVariant}
+                  whileHover="hover"
+                  transition="transition"
+                  style={{
+                    border: content && `2px solid ${content.videoBorderColor}`,
+                    boxShadow:
+                      content && `6px 6px 6px ${content.videoBoxShadowColor}`
+                  }}
+                >
+                  <Player
+                    poster={content && content.videoThumbnail}
+                    src={content && content.videoUrl}
+                  ></Player>
+                </motion.div>
+              </div>
+            </section>
+            <section>
+              <div
+                className={`row container align-center space-evenly ${styles.wrapper}`}
+              >
+                <div className={styles.info_wrapper}>
+                  <motion.div whileHover={{ scale: 0.9 }}>
+                    <div>
+                      {content && (
+                        <div className="ql-editor">
+                          {parse(content.sectionOneHeading)}
+                        </div>
+                      )}
+                      {content && (
+                        <div className="ql-editor">
+                          {parse(content.sectionOneText)}
+                        </div>
+                      )}
+                    </div>
+                  </motion.div>
+                  <div className={styles.image_container}>
+                    <motion.img
+                      whileHover={{ scale: 0.8 }}
+                      drag
+                      dragConstraints={{
+                        top: 10,
+                        left: 10,
+                        right: 10,
+                        bottom: 10
+                      }}
+                      dragElastic={1}
+                      src={content && content.sectionOneImage}
+                      className={styles.logo}
+                      alt="Section One"
+                    />
+                  </div>
+                </div>
+                <div className={styles.info_wrapper}>
+                  <div className={styles.image_container}>
+                    <img
+                      src={content && content.sectionTwoImage}
+                      className={styles.image}
+                      alt="Section Two"
+                    />
+                  </div>
                   <div>
-                    {content ? (
+                    {content && (
                       <div className="ql-editor">
-                        {parse(content.sectionOneHeading)}
+                        {parse(content.sectionTwoHeading)}
                       </div>
-                    ) : (
-                      <h1>What we do</h1>
                     )}
-                    {content ? (
+                    {content && (
                       <div className="ql-editor">
-                        {parse(content.sectionOneText)}
+                        {parse(content.sectionTwoText)}
                       </div>
-                    ) : (
-                      <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Magni consectetur, dolor illum aperiam repellat
-                        numquam iure, laudantium sunt tenetur aut quasi eum vel
-                        maxime deleniti corporis quod nemo totam optio.
-                      </p>
                     )}
                   </div>
-                </motion.div>
-                <div className={styles.image_container}>
-                  <motion.img
-                    whileHover={{ scale: 0.8 }}
-                    drag
-                    dragConstraints={{
-                      top: 10,
-                      left: 10,
-                      right: 10,
-                      bottom: 10
-                    }}
-                    dragElastic={1}
-                    src="/logo512.png"
-                    className={styles.logo}
-                    alt="Nerdhub Logo"
-                  />
                 </div>
               </div>
-              <div className={styles.info_wrapper}>
-                <div className={styles.image_container}>
-                  <img
-                    src="/images/gaming_room.jpeg"
-                    className={styles.image}
-                    alt="Nerdhub Logo"
-                  />
-                </div>
-                <div>
-                  {content ? (
-                    <div className="ql-editor">
-                      {parse(content.sectionTwoHeading)}
-                    </div>
-                  ) : (
-                    <h1>Our Mission</h1>
-                  )}
-                  {content ? (
-                    <div className="ql-editor">
-                      {parse(content.sectionTwoText)}
-                    </div>
-                  ) : (
-                    <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Magni consectetur, dolor illum aperiam repellat numquam
-                      iure, laudantium sunt tenetur aut quasi eum vel maxime
-                      deleniti corporis quod nemo totam optio.
-                    </p>
-                  )}
-                </div>
-              </div>
-            </div>
-          </section>
+            </section>
+          </motion.div>
         </motion.div>
-      </motion.div>
+      )}
       <MediaQuery minWidth={800}>
         <Footer></Footer>
       </MediaQuery>
