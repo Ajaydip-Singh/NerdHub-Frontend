a2b1ca0c5b3b81dcdcf649a42bf59f7274cfd09a
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-10 10:28:24 +0300

    :sparkles: feat: configure product to show styles from backend

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import Rating from '../Rating/Rating';
 import styles from './Product.module.css';
 import { motion } from 'framer-motion';
+import parse from 'html-react-parser';
+import { stripHtml } from '../../utils';
 
 export default function Product(props) {
   const { product } = props;
@@ -10,17 +12,28 @@ export default function Product(props) {
     <motion.div
       key={product._id}
       className={styles.card}
+      style={{
+        border: `2px solid ${product.borderColor}`,
+        backgroundColor: product.backgroundColor
+      }}
       whileHover={{
-        scale: 1.02
+        scale: 1.03,
+        borderColor: product.borderHoverColor
       }}
     >
       <Link to={`/shop/product/${product._id}`}>
-        <img className={styles.medium} src={product.image} alt={product.name} />
+        <img
+          className={styles.medium}
+          src={product.image}
+          alt={stripHtml(product.name)}
+        />
       </Link>
       <div className={styles.card_body}>
-        <h2 className={styles.product_name}>{product.name}</h2>
+        <div className="ql-editor">
+          <p className={styles.product_name}>{parse(product.name)}</p>
+        </div>
         <Rating rating={product.rating} numReviews={product.numReviews} />
-        <div className={styles.price}>${product.price}</div>
+        <div className="ql-editor">{parse(product.displayPrice)}</div>
       </div>
     </motion.div>
   );
