467f14ebbff489918066d14905d3d23933b8dc00
Ajaydip Singh <singh@minerva.kgi.edu>
2021-09-16 12:57:18 +0300

    :sparkles: feat: show a 3s banner when product added to cart

--- src/screens/PublicScreens/ProductScreen/ProductScreen.js
+++ src/screens/PublicScreens/ProductScreen/ProductScreen.js
@@ -24,6 +24,7 @@ export default function ProductScreen(props) {
   const productId = props.match.params.id;
 
   const [quantity, setQuantity] = useState(1);
+  const [showDiv, setShowDiv] = useState(false);
 
   const productGetSlice = useSelector((state) => state.productGetSlice);
   const { status, product, error } = productGetSlice;
@@ -31,7 +32,11 @@ export default function ProductScreen(props) {
   const dispatch = useDispatch();
   const addToCartHandler = () => {
     // props.history.push(`/cart/${productId}?quantity=${quantity}`);
-    dispatch(addToCart({productId, quantity}));
+    dispatch(addToCart({ productId, quantity }));
+    setShowDiv(true);
+    setTimeout(() => {
+      setShowDiv(false);
+    }, 3000);
   };
 
   useEffect(() => {
@@ -90,55 +95,71 @@ export default function ProductScreen(props) {
                 {product && parse(product.description)}
               </div>
             </div>
-            <div className={styles.checkout}>
-              <table className="table">
-                <tbody>
-                  <tr>
-                    <td>Price:</td>
-                    <td>KES {product && product.price}</td>
-                  </tr>
-                  <tr>
-                    <td>Status:</td>
-                    <td>
-                      {product && product.countInStock > 0 ? (
-                        <span className="success">Available</span>
-                      ) : (
-                        <span className="error">Not In Stock</span>
-                      )}
-                    </td>
-                  </tr>
-                  {product && product.countInStock > 0 ? (
+            <div>
+              <div className={styles.checkout}>
+                <table className="table">
+                  <tbody>
                     <tr>
-                      <td>Quantity:</td>
+                      <td>Price:</td>
+                      <td>KES {product && product.price}</td>
+                    </tr>
+                    <tr>
+                      <td>Status:</td>
                       <td>
-                        <select
-                          value={quantity}
-                          onChange={(e) => setQuantity(e.target.value)}
-                        >
-                          {[
-                            ...Array(product && product.countInStock).keys()
-                          ].map((x) => (
-                            <option key={x + 1} value={x + 1}>
-                              {x + 1}
-                            </option>
-                          ))}
-                        </select>
+                        {product && product.countInStock > 0 ? (
+                          <span className="success">Available</span>
+                        ) : (
+                          <span className="error">Not In Stock</span>
+                        )}
                       </td>
                     </tr>
-                  ) : (
-                    ''
-                  )}
-                </tbody>
-              </table>
-              <button
-                onClick={addToCartHandler}
-                disabled={product && product.countInStock === 0}
-                className={`button border_bottom ${styles.cart_button}`}
+                    {product && product.countInStock > 0 ? (
+                      <tr>
+                        <td>Quantity:</td>
+                        <td>
+                          <select
+                            value={quantity}
+                            onChange={(e) => setQuantity(e.target.value)}
+                          >
+                            {[
+                              ...Array(product && product.countInStock).keys()
+                            ].map((x) => (
+                              <option key={x + 1} value={x + 1}>
+                                {x + 1}
+                              </option>
+                            ))}
+                          </select>
+                        </td>
+                      </tr>
+                    ) : (
+                      ''
+                    )}
+                  </tbody>
+                </table>
+                <button
+                  onClick={addToCartHandler}
+                  disabled={product && product.countInStock === 0}
+                  className={`button border_bottom ${styles.cart_button}`}
+                >
+                  Add to Cart
+                </button>
+              </div>
+              <motion.div
+                className={`${styles.floating_div} ${
+                  showDiv ? styles.show_div : styles.hide_div
+                }`}
               >
-                Add to Cart
-              </button>
+                {/* Product Added to Cart */}
+                <MessageBox variant="success">
+                  Product Added to{' '}
+                  <Link className={styles.cart_link} to="/shop/cart">
+                    Cart.
+                  </Link>
+                </MessageBox>
+              </motion.div>
             </div>
           </motion.div>
+
           <MediaQuery minWidth={800}>
             <Footer></Footer>
           </MediaQuery>
