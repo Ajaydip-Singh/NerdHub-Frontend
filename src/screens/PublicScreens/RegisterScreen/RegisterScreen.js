e5b263dd05529b7d36b3168d12fcee4cbd1fec53
Ajaydip Singh <singh@minerva.kgi.edu>
2021-10-18 21:21:15 +0300

    :recycle: refactor: set register styling from admin

--- src/screens/PublicScreens/RegisterScreen/RegisterScreen.js
+++ src/screens/PublicScreens/RegisterScreen/RegisterScreen.js
@@ -8,7 +8,11 @@ import styles from './RegisterScreen.module.css';
 import LoadingBox from '../../../components/LoadingBox/LoadingBox';
 import MessageBox from '../../../components/MessageBox/MessageBox';
 import { registerUser } from '../../../slices/userSlices/userRegisterSlice';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getRegisterPageContent } from '../../../slices/pageSlices/registerPageContentSlices/registerPageContentGetSlice';
+import parse from 'html-react-parser';
+import { pageVariant } from '../../../animate';
+import { motion } from 'framer-motion';
 
 export default function RegisterScreen(props) {
   const isSmallerScreen = useMediaQuery({ query: '(max-width: 767px)' });
@@ -73,10 +77,23 @@ export default function RegisterScreen(props) {
     }
   };
 
+  const registerPageContentGetSlice = useSelector(
+    (state) => state.registerPageContentGetSlice
+  );
+  const {
+    status: statusContent,
+    content,
+    error: errorContent
+  } = registerPageContentGetSlice;
+
   const userRegister = useSelector((state) => state.userRegister);
   const { status, createdUser, error } = userRegister;
 
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRegisterPageContent({}));
+  }, [dispatch]);
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
     dispatch(registerUser({ firstName, lastName, email, password }));
@@ -85,113 +102,194 @@ export default function RegisterScreen(props) {
   return (
     <div>
       <Header register></Header>
-      <section className={`row ${styles.main_wrapper}`}>
-        <div
-          className={`col-md-6 ${styles.info_box}`}
-          style={{
-            backgroundImage: 'url(/images/man_with_gun.jpeg)'
-          }}
-        ></div>
-        <div
-          className={`col-md-6 ${styles.login_box}`}
-          style={
-            isSmallerScreen
-              ? {
-                  backgroundImage: 'url(/images/man_with_gun.jpeg)'
-                }
-              : {}
-          }
-        >
-          <h1 className={styles.title}>Register</h1>
-          {error && <MessageBox variant="danger">{error}</MessageBox>}
-          {createdUser && (
-            <MessageBox variant="success">
-              Success. Verify Email In Inbox.
-            </MessageBox>
-          )}
-          <form onSubmit={onSubmitHandler} className={styles.form}>
-            <div>
-              {firstNameError && (
-                <MessageBox validation>{firstNameError}</MessageBox>
-              )}
-              <input
-                className={`${styles.input} ${
-                  firstNameError ? `${styles.val_danger}` : ``
-                }`}
-                placeholder="Enter first name"
-                type="text"
-                name="first_name"
-                onChange={(e) => validateFirstName(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              {lastNameError && (
-                <MessageBox validation>{lastNameError}</MessageBox>
-              )}
-              <input
-                className={`${styles.input} ${
-                  lastNameError ? `${styles.val_danger}` : ``
-                }`}
-                placeholder="Enter last name"
-                type="text"
-                name="last_name"
-                onChange={(e) => validateLastName(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              {emailError && <MessageBox validation>{emailError}</MessageBox>}
-              <input
-                className={`${styles.input} ${
-                  emailError ? `${styles.val_danger}` : ``
-                }`}
-                placeholder="Enter email"
-                type="text"
-                name="email"
-                onChange={(e) => validateEmail(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              {passwordError && (
-                <MessageBox validation>{passwordError}</MessageBox>
-              )}
-              <input
-                className={`${styles.input} ${
-                  passwordError ? `${styles.val_danger}` : ``
-                }`}
-                placeholder="Enter password"
-                type="password"
-                name="password"
-                onChange={(e) => validatePassword(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              {confirmPasswordError && (
-                <MessageBox validation>{confirmPasswordError}</MessageBox>
-              )}
-              <input
-                className={`${styles.input} ${
-                  confirmPasswordError ? `${styles.val_danger}` : ``
-                }`}
-                placeholder="Confirm password"
-                type="password"
-                name="confirm_password"
-                onChange={(e) => validateConfirmPassword(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <button className={styles.submit_button} type="submit">
-                {status === 'loading' ? <LoadingBox></LoadingBox> : `Register`}
-              </button>
-            </div>
-            <p className="mt-1">
-              Already have an account?{' '}
-              <Link className={`border_bottom ${styles.link}`} to="/login">
-                Login
-              </Link>
-            </p>
-          </form>
+      {statusContent === 'loading' ? (
+        <div className="min_page_height">
+          <LoadingBox></LoadingBox>
         </div>
-      </section>
+      ) : errorContent ? (
+        <div className="min_page_height">
+          <MessageBox variant="danger">
+            Oops. We are temporarily unavailable. Please try again later.
+          </MessageBox>
+        </div>
+      ) : (
+        <motion.section
+          variants={pageVariant}
+          initial="initial"
+          animate="final"
+          className={`row ${styles.main_wrapper}`}
+        >
+          <div
+            className={`col-md-6 ${styles.info_box}`}
+            style={{
+              backgroundImage: `url(${content && content.mainBackgroundImage})`
+            }}
+          ></div>
+          <div
+            className={`col-md-6 ${styles.login_box}`}
+            style={
+              isSmallerScreen
+                ? {
+                    backgroundImage: `url(${
+                      content && content.mainBackgroundImage
+                    })`
+                  }
+                : { backgroundColor: content && content.mainBackgroundColor }
+            }
+          >
+            <div className="ql-editor">
+              <div className={styles.title}>
+                {content && parse(content.mainHeading)}
+              </div>
+            </div>
+            {error && <MessageBox variant="danger">{error}</MessageBox>}
+            {createdUser && (
+              <MessageBox variant="success">
+                Success. Verify Email In Inbox.
+              </MessageBox>
+            )}
+            <form onSubmit={onSubmitHandler} className={styles.form}>
+              <div>
+                {firstNameError && (
+                  <MessageBox validation>{firstNameError}</MessageBox>
+                )}
+                <input
+                  style={{
+                    backgroundColor: content && content.inputBackgroundColor,
+                    color: content && content.inputTextColor,
+                    outline: `1px solid ${content && content.inputBorderColor}`,
+                    border: `2px solid ${content && content.inputBorderColor}`
+                  }}
+                  className={`${styles.input} ${
+                    firstNameError ? `${styles.val_danger}` : ``
+                  }`}
+                  placeholder="Enter first name"
+                  type="text"
+                  name="first_name"
+                  onChange={(e) => validateFirstName(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                {lastNameError && (
+                  <MessageBox validation>{lastNameError}</MessageBox>
+                )}
+                <input
+                  style={{
+                    backgroundColor: content && content.inputBackgroundColor,
+                    color: content && content.inputTextColor,
+                    outline: `1px solid ${content && content.inputBorderColor}`,
+                    border: `2px solid ${content && content.inputBorderColor}`
+                  }}
+                  className={`${styles.input} ${
+                    lastNameError ? `${styles.val_danger}` : ``
+                  }`}
+                  placeholder="Enter last name"
+                  type="text"
+                  name="last_name"
+                  onChange={(e) => validateLastName(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                {emailError && <MessageBox validation>{emailError}</MessageBox>}
+                <input
+                  style={{
+                    backgroundColor: content && content.inputBackgroundColor,
+                    color: content && content.inputTextColor,
+                    outline: `1px solid ${content && content.inputBorderColor}`,
+                    border: `2px solid ${content && content.inputBorderColor}`
+                  }}
+                  className={`${styles.input} ${
+                    emailError ? `${styles.val_danger}` : ``
+                  }`}
+                  placeholder="Enter email"
+                  type="text"
+                  name="email"
+                  onChange={(e) => validateEmail(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                {passwordError && (
+                  <MessageBox validation>{passwordError}</MessageBox>
+                )}
+                <input
+                  style={{
+                    backgroundColor: content && content.inputBackgroundColor,
+                    color: content && content.inputTextColor,
+                    outline: `1px solid ${content && content.inputBorderColor}`,
+                    border: `2px solid ${content && content.inputBorderColor}`
+                  }}
+                  className={`${styles.input} ${
+                    passwordError ? `${styles.val_danger}` : ``
+                  }`}
+                  placeholder="Enter password"
+                  type="password"
+                  name="password"
+                  onChange={(e) => validatePassword(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                {confirmPasswordError && (
+                  <MessageBox validation>{confirmPasswordError}</MessageBox>
+                )}
+                <input
+                  style={{
+                    backgroundColor: content && content.inputBackgroundColor,
+                    color: content && content.inputTextColor,
+                    outline: `1px solid ${content && content.inputBorderColor}`,
+                    border: `2px solid ${content && content.inputBorderColor}`
+                  }}
+                  className={`${styles.input} ${
+                    confirmPasswordError ? `${styles.val_danger}` : ``
+                  }`}
+                  placeholder="Confirm password"
+                  type="password"
+                  name="confirm_password"
+                  onChange={(e) => validateConfirmPassword(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <button
+                  style={{
+                    '--button-text-color':
+                      content && content.registerButtonTextColor,
+                    '--button-background-color':
+                      content && content.registerButtonBackgroundColor,
+                    '--button-border-color':
+                      content && content.registerButtonTextColor
+                  }}
+                  className={styles.submit_button}
+                  type="submit"
+                >
+                  {status === 'loading' ? (
+                    <LoadingBox></LoadingBox>
+                  ) : (
+                    `Register`
+                  )}
+                </button>
+              </div>
+              <p className="mt-1">
+                <div className="ql-editor mb-2">
+                  {content && parse(content.newAccountText)}
+                </div>
+                <Link
+                  style={{
+                    '--link-text-color': content && content.signUpLinkTextColor,
+                    '--link-background-color':
+                      content && content.signUpLinkBackgroundColor,
+                    '--link-border-color':
+                      content && content.signUpLinkBorderColor
+                  }}
+                  className={`border_bottom ${styles.link}`}
+                  to="/login"
+                >
+                  Login
+                </Link>
+              </p>
+            </form>
+          </div>
+        </motion.section>
+      )}
+
       <MediaQuery maxWidth={800}>
         <BottomNav></BottomNav>
       </MediaQuery>
